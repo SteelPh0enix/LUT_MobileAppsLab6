8d0f8745e6ac17eb3f82915a136356f92286e811
SteelPh0enix <steelphoenix2137@gmail.com>
2020-11-09 12:50:09 +0100

    first commit

---
+++ app/src/main/java/com/example/basiclab/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.basiclab
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_test_a -> {
+                Snackbar.make(findViewById(R.id.fab), "Hello there, A", Snackbar.LENGTH_LONG).show()
+            }
+            R.id.action_test_b -> {
+                Snackbar.make(findViewById(R.id.fab), "Hi, B", Snackbar.LENGTH_LONG).show()
+            }
+        }
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            R.id.action_test_a -> true
+            R.id.action_test_b -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
